a40807cefcb4a752bbd29bbb1052a04f5986a32b
Scar2345 <145857731+Scar2345@users.noreply.github.com>
2025-08-03 01:06:14 +0530

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Add these two lines:
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "98.css/dist/98.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
